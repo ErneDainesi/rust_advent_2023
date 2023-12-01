cbdbcfb3fc998fdb9879608d5ee4ae5e5798a67f
ErneDainesi <edainesi@fi.uba.ar>
2023-12-01 18:54:46 -0300

    finish part one day one

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,29 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    Some(
+        input
+            .split('\n')
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|s| s.chars().filter(|c| c.is_numeric()).collect())
+            .collect::<Vec<String>>()
+            .iter_mut()
+            .map(|s| {
+                if s.len() > 2 {
+                    let new_value = format!("{}{}", s.chars().next().unwrap(), s.chars().last().unwrap());
+                    *s = new_value;
+                } else if s.len() == 1 {
+                    let first_char: char = s.chars().next().unwrap();
+                    let new_value = format!("{}{}", first_char, first_char);
+                    *s = new_value;
+                }
+                return s.parse::<u32>().unwrap();
+            })
+            .collect::<Vec<u32>>()
+            .iter()
+            .sum::<u32>()
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
