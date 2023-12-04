d80a57867a0784a18f63da8e0cedc0421ef15c1c
ErneDainesi <edainesi@fi.uba.ar>
2023-12-04 02:46:59 -0300

    day 2 part 1

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,7 +1,60 @@
 advent_of_code::solution!(2);
 
+#[derive(Debug)]
+struct Game {
+    pub id: usize,
+    pub sets: Vec<GameSet>
+}
+
+#[derive(Debug)]
+struct GameSet {
+    pub blue: u32,
+    pub green: u32,
+    pub red: u32
+}
+
+impl GameSet {
+    pub fn is_valid_set(&self) -> bool {
+        let (max_red, max_green, max_blue) = (12, 13, 14);
+        self.red <= max_red && self.green <= max_green && self.blue <= max_blue
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut games = vec![];
+    for (i, line) in input.lines().enumerate() {
+        let mut game = Game {id: i + 1, sets: vec![]};
+        let game_prefix = format!("Game {}: ", i + 1);
+        let line = line.strip_prefix(&game_prefix).unwrap();
+        for game_set in line.split(';').collect::<Vec<&str>>() {
+            let mut set = GameSet {blue: 0, green: 0, red: 0};
+            for dice in game_set.split(',').collect::<Vec<&str>>() {
+                let dice_and_amount: Vec<&str> = dice.trim().split(' ').collect();
+                let pair: (u32, &str) = (dice_and_amount[0].parse::<u32>().unwrap(), dice_and_amount[1]);
+                match pair.1 {
+                    "blue" => set.blue += pair.0,
+                    "green" => set.green += pair.0,
+                    "red" => set.red += pair.0,
+                    _ => continue,
+                }
+            }
+            game.sets.push(set);
+        }
+        games.push(game);
+    }
+    let mut res = 0;
+    for game in games.iter() {
+        let mut is_valid_set = true;
+        for set in game.sets.iter() {
+            if !set.is_valid_set() {
+                is_valid_set = false;
+            }
+        }
+        if is_valid_set {
+            res += game.id;
+        }
+    }
+    Some(res as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
